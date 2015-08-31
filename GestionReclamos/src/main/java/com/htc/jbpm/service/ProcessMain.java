ffb4d9016ab9c6ab97df52558034555a37bec2a4
unknown <emena@hightech-corp.com>
2015-08-31 16:01:07 -0600

    carga inicial

---
+++ GestionReclamos/src/main/java/com/htc/jbpm/service/ProcessMain.java
@@ -0,0 +1,56 @@
+package com.htc.jbpm.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.jbpm.test.JBPMHelper;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+
+public class ProcessMain {
+	
+	public static void main(String[] args) {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieBase kbase = kContainer.getKieBase("kbase");
+
+		RuntimeManager manager = createRuntimeManager(kbase);
+		RuntimeEngine engine = manager.getRuntimeEngine(null);
+		KieSession ksession = engine.getKieSession();
+		TaskService taskService = engine.getTaskService();
+
+		ksession.startProcess("com.htc.jbpm.process.reclamo");
+
+		// let john execute Task 1
+		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner("krisv", "en-UK");
+		TaskSummary task = list.get(0);
+		System.out.println("krisv is executing task " + task.getName());
+		taskService.start(task.getId(), "krisv");
+		taskService.complete(task.getId(), "krisv", null);
+
+		manager.disposeRuntimeEngine(engine);
+		System.exit(0);
+	}
+
+	private static RuntimeManager createRuntimeManager(KieBase kbase) {
+		JBPMHelper.startH2Server();
+		JBPMHelper.setupDataSource();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
+		RuntimeEnvironmentBuilder builder = RuntimeEnvironmentBuilder.Factory.get()
+			.newDefaultBuilder().entityManagerFactory(emf)
+			.knowledgeBase(kbase);
+		return RuntimeManagerFactory.Factory.get()
+			.newSingletonRuntimeManager(builder.get(), "com.sample:example:1.0");
+	}
+
+}
